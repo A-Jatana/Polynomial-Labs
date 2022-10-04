a316b9c290c512588f9fff4c9b0639448a36ae88
amitoz jatana <amitoz2003@hotmail.com>
2022-10-03 20:46:18 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,52 @@
+package lab2;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Driver { 
- public static void main(String [] args) { 
-  Polynomial p = new Polynomial(); 
-  System.out.println(p.evaluate(3)); 
-  double [] c1 = {6,0,0,5}; 
-  Polynomial p1 = new Polynomial(c1); 
-  double [] c2 = {0,-2,0,0,-9}; 
-  Polynomial p2 = new Polynomial(c2); 
-  Polynomial s = p1.add(p2); 
-  System.out.println("s(0.1) = " + s.evaluate(0.1)); 
-  if(s.hasRoot(1)) 
-   System.out.println("1 is a root of s"); 
-  else 
-   System.out.println("1 is not a root of s"); 
- } 
-} 
+    public static void main(String [] args) throws IOException  {  
+          
+          double [] c1 = {6,-2,5};
+          int [] e1 = {0,1,3}; 
+          Polynomial p1 = new Polynomial(c1, e1);
+          double [] c2 = {5,-3,7}; 
+          int [] e2 = {0,2,8};
+          Polynomial p2 = new Polynomial(c2, e2); 
+          double [] c3 = {4,2}; 
+          int [] e3 = {1,2};
+          Polynomial p3 = new Polynomial(c3, e3); 
+          double [] c4 = {8,2}; 
+          int [] e4 = {0,1};
+          Polynomial p4 = new Polynomial(c4, e4); 
+          double [] c5 = {1, 1};
+          int [] e5 = {1, 0};
+          Polynomial p5 = new Polynomial(c5, e5);
+          double [] c6 = {1, -1};
+          int [] e6 = {1, 0};
+          Polynomial p6 = new Polynomial(c6, e6);
+          double [] c7 = {5,2,1};
+          int [] e7 = {0,1,2}; 
+          Polynomial p7 = new Polynomial(c7, e7);
+          double [] c8 = {2,3,4};
+          int [] e8 = {0,2,4}; 
+          Polynomial p8 = new Polynomial(c8, e8);
+          File expression = new File("C:\\Users\\amito\\expression.txt");
+          Polynomial p9 = new Polynomial(expression);
+
+
+          Polynomial s = p1.add(p2); 
+          Polynomial t = p1.multiply(p2);
+
+          System.out.println(Arrays.toString(t.poly)); 
+          System.out.println(Arrays.toString(t.expo));
+          System.out.println(p1.evaluate(3)); 
+          System.out.println(Arrays.toString(p9.poly)); 
+          System.out.println(Arrays.toString(p9.expo));
+          p5.saveToFile("check.txt");
+          if(p4.hasRoot(-4)) 
+           System.out.println("This is a root of s"); 
+          else 
+          System.out.println("This is not a root of s"); 
+         } 
+    }
--- Polynomial.java
+++ Polynomial.java
@@ -1,43 +1,172 @@
-public class Polynomial {
+package lab2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Polynomial 
+{
 	
 	double[] poly;
+	int [] expo;
 	
-	public Polynomial() {
-		poly = new double[] {0};
+	public Polynomial() 
+	{
+
+		poly = null;
+		expo = null;
 	}
 
-	public Polynomial(double[] arr) {
-		int len = arr.length;
-		poly = new double[len];
-		for(int i = 0; i < len; i++) {
-            	poly[i] = arr[i];
-		}
+	public Polynomial(double[] co_arr, int[] ex_arr)
+	{
+		this.poly = co_arr;
+		this.expo = ex_arr;
 	}
-
-	public Polynomial add(Polynomial other) {
-		int len = poly.length;
-		int other_len = other.poly.length;
-		double hold;
-		if(len >= other_len) {
-			for(int i = 0; i < other_len; i++) {
-				poly[i] = poly[i] + other.poly[i];
+	
+	public Polynomial(File p_file) throws FileNotFoundException
+	{
+		Scanner inp = new Scanner(p_file);
+		String expression = inp.next();	
+		inp.close();
+		expression = expression.replace("-", "~-");
+		String [] parts = expression.split("~|\\+");
+		
+		int lgth = parts.length;
+		double [] co = new double[lgth];
+		int [] ex = new int[lgth];
+		
+		for(int i = 0; i < lgth; i++)
+		{
+			if(parts[i].contains("x") == false)
+			{
+				double x = Double.parseDouble(parts[i]);
+				ex[i] = 0;
+				co[i] = x;
 			}
-		return this;
-		}
-		else {
-			for(int i = 0; i < len; i++) {
-				other.poly[i] = other.poly[i] + poly[i];
+				
+			else
+			{
+				String [] fin = parts[i].split("x");
+				double y =  Double.parseDouble(fin[0]);
+				int z = Integer.parseInt(fin[1]);
+				ex[i] = z;
+				co[i] = y;
 			}
-		return other;
 		}
+		
+		poly = co;
+		expo = ex;
+	}
+	
+	public int find_len(Polynomial one, Polynomial two) 
+	{
+		int len = one.expo.length;
+		int other_len = two.expo.length;
+		int new_len = len + other_len;
+		for(int i = 0; i < len; i++) 
+		{
+			for(int j = 0; j < other_len; j++)
+			{
+				if(one.expo[i] == two.expo[j] && new_len > 1)
+				{
+					new_len--;
+				}
+				
+				if(one.expo[i] == two.expo[j] && one.poly[i] + two.poly[j] == 0&& new_len > 1)
+				{
+					new_len--;
+				}
+			}
+		}
+		
+		
+		return new_len;
+	}
+	
+	public int contains1(int [] arr, int check, int len)
+	{
+		for(int i = 0; i < len; i++)
+		{
+			if(arr[i] == check)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public Polynomial add(Polynomial other) 
+	{	
+		int lgth = find_len(this, other);
+		Polynomial ret = new Polynomial();
+		ret.poly = new double[lgth];
+		ret.expo = new int[lgth];
+		
+		for(int i = 0; i < lgth; i++)
+		{
+			ret.poly[i] = -1;
+			ret.expo[i] = -1;
+		}
+	
+		int len = this.expo.length;
+		int other_len = other.expo.length;
+		int c = 0;
+		int e = 0;
+		
+		for(int i = 0; i < len; i++) 
+		{
+			for(int j = 0; j < other_len; j++)
+			{
+				if(this.expo[i] == other.expo[j] && c < lgth && e < lgth)
+				{
+					
+					
+					if(contains1(ret.expo, this.expo[i], lgth) != -1) 
+					{
+						int ind = contains1(ret.expo, this.expo[i], lgth);
+						ret.poly[ind] = this.poly[i] + other.poly[j];
+					}
+					else
+					{
+						
+						ret.poly[c] = this.poly[i] + other.poly[j];
+						ret.expo[e] = this.expo[e];
+						c++;
+						e++;
+					}		
+				}
+				else if(this.expo[i] != other.expo[j] && c < lgth && e < lgth)
+				{
+					if(contains1(ret.expo, this.expo[i], lgth) == -1)
+					{	
+						ret.poly[c] = this.poly[i];
+						c++;
+						ret.expo[e] = this.expo[i];
+						e++;
+					}
+					if(contains1(ret.expo, other.expo[j], lgth) == -1)
+					{
+						ret.poly[c] = other.poly[j];
+						c++;
+						ret.expo[e] = other.expo[j];
+						e++;	
+					}
+				}
+			}
+		}
+		return ret;
 	}
 
-	public double evaluate(double x) {
+
+	public double evaluate(double x) 
+	{
 		double total = 0.0;
 		int len = poly.length;
 		double val;
 		for(int i = 0; i < len; i++) {
-			val = Math.pow(x, i);
+			val = Math.pow(x, expo[i]);
 			total = (poly[i] * val) + total;
 		}
 		return total;
@@ -46,7 +175,148 @@ public class Polynomial {
 	public boolean hasRoot(double r) {
 		return evaluate(r) == 0.0;
 	}
-}
-			
 	
+	
+	public int find_len_mult(Polynomial one, Polynomial two) 
+	{
+		int len = one.expo.length;
+		int other_len = two.expo.length;
+		int new_len = len * other_len;
+		
+		for(int i = 0; i < len; i++) 
+		{
+			for(int j = 0; j < other_len; j++)
+			{
+				if(one.expo[i] == two.expo[j] && new_len > 1)
+				{
+					new_len--;
+				}
+			}
+		}
+		
+		return new_len;
+	}
+	
+	public Polynomial multiply(Polynomial other)
+	{	
+		int len = this.expo.length;
+		int other_len = other.expo.length;
+		int lgth = find_len_mult(this, other);
+		Polynomial ret = new Polynomial();
+		ret.poly = new double[lgth];
+		ret.expo = new int[lgth];
+		
+		for(int i = 0; i < lgth; i++)
+		{
+			ret.poly[i] = -1;
+			ret.expo[i] = -1;
+		}
+		int c = 0;
+		
+		for(int i = 0; i < len; i++) 
+		{
+			for(int j = 0; j < other_len; j++)
+			{
+				int check = this.expo[i] + other.expo[j];
+				
+				if(contains1(ret.expo, check, lgth) != -1)
+				{
+					int ind = contains1(ret.expo, check, lgth);
+					ret.poly[ind] += this.poly[i] * other.poly[j];
+						
+				}
+				
+				else if(c < lgth)
+				{
+					ret.poly[c] = this.poly[i] * other.poly[j];
+					ret.expo[c] = this.expo[i] + other.expo[j];
+					c++;
+				}
+			}
+		}
+		
+		return ret;
+	}
+	
+	public void saveToFile(String file_name) throws IOException
+	{
+		int len = this.poly.length;
+		File f = new File(file_name);
+		
+	
+		if(f.exists())
+		{
+			FileWriter writer = new FileWriter(f);
+			for(int i = 0; i < len; i++)
+			{
+				double hold = this.poly[i];
+				String str = Double.toString(hold);
+				int hold2 = this.expo[i];
+				String str2 = Integer.toString(hold2);
+				
+				if(hold2 == 0)
+				{
+					writer.write(str);
+				}
+				
+				else if(str.contains("-") == true)
+				{
+					
+					writer.write(str);
+					writer.write("x");
+					writer.write(str2);
+				}
+				
+				else
+				{
+					writer.write("+");
+					writer.write(str);
+					writer.write("x");
+					writer.write(str2);
+				}
+				
+			}
 			
+			writer.close();
+		}
+		
+		else if(!f.exists())
+		{
+			FileWriter writer = new FileWriter(f);
+			for(int i = 0; i < len; i++)
+			{
+				double hold = this.poly[i];
+				String str = Double.toString(hold);
+				int hold2 = this.expo[i];
+				String str2 = Integer.toString(hold2);
+				
+				if(hold2 == 0)
+				{
+					writer.write(str);
+				}
+				
+				else if(str.contains("-") == true)
+				{
+					
+					writer.write(str);
+					writer.write("x");
+					writer.write(str2);
+				}
+				
+				else
+				{
+					writer.write("+");
+					writer.write(str);
+					writer.write("x");
+					writer.write(str2);
+				}
+				
+			}
+			
+			writer.close();
+		}
+		
+	}
+	
+	
+}
